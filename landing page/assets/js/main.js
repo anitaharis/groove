5d41199cba5b539132bf6735b44441c56b54f7df
anitaharis <anitaharis@hotmail.com>
2021-01-23 00:15:11 -0800

    Add files via upload

---
+++ landing page/assets/js/main.js
@@ -0,0 +1,36 @@
+
+!(function($) {
+  "use strict";
+
+
+  // Activate smooth scroll on page load with hash links
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+ 
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+})
+
